fff59b38aeb3dfdbeef7778b7527bb393f8e987f
FoggyCaligo <136309172+FoggyCaligo@users.noreply.github.com>
2024-06-12 19:08:21 +0900

    update

---
+++ Project/test.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html lang="ko">
+
+ 
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>PHP Syntax</title>
+    
+</head>
+
+ 
+
+<body>
+
+<?php
+    echo "PHP 수업에 잘 오셨습니다!"
+?>
+
+</body>
+
+ 
+
+</html>
